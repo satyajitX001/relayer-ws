29dd5a04631087318408cbeef801724cf2ea6fef
satyajitX001 <satyajitray1201@gmail.com>
2025-07-20 19:20:25 +0530

    boiler palate code for relayer

--- relayer-ws/index.ts
+++ relayer-ws/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+
+    ws.send('something');
+});
